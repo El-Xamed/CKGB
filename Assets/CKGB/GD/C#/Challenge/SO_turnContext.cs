bce2ffbad906119a66cbb3fc3a4109471d3dcc3d
El-Xamed <maximetessier47@gmail.com>
2024-01-25 11:16:42 +0100

    add SO context pour passer une etape

---
+++ Assets/CKGB/GD/C#/Challenge/SO_turnContext.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SO_turnContext : ScriptableObject
+{
+    SO_ActionClass playedAction;
+    Proto_SO_Character actor;
+    int position;
+    SO_Accessories currentAccessoire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
