1c0c5297bd7360a48ca0fd2de6338584f99c4c45
El-Xamed <maximetessier47@gmail.com>
2024-02-01 15:57:27 +0100

    systeme de tours temps mort fonctionnel

--- Assets/CKGB/GD/C#/TempsMort/T_TempsMort.cs
+++ Assets/CKGB/GD/C#/TempsMort/T_TempsMort.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class T_TempsMort : MonoBehaviour
 {
@@ -12,16 +13,27 @@ public class T_TempsMort : MonoBehaviour
     [SerializeField]EventSystem Es;
 
     [SerializeField]
-    Image[] charactersLittleResume;
+    GameObject[] charactersLittleResume;
 
     [SerializeField]
-    Image[] charactersCompleteResume;
+    GameObject[] charactersCompleteResume;
 
     [SerializeField]
-    Button[] charactersButton;
+    TMP_Text[] littleCharactersSpecs;
+    [SerializeField]
+    TMP_Text[] CompleteCharactersSpecs1;
+    [SerializeField]
+    TMP_Text[] CompleteCharactersSpecs2;
+
+    [SerializeField]
+    GameObject[] charactersButton;
+    [SerializeField]
+    bool[] characterHasPlayed;
 
     [SerializeField]
     GameObject[] actions;
+    [SerializeField]
+    GameObject ChallengeButton;
 
     [SerializeField]
     List<StartPosition> listCharactersPositions;
@@ -32,6 +44,7 @@ public class T_TempsMort : MonoBehaviour
     PlayerInput pi;
 
     [SerializeField]GameObject currentButton;
+    [SerializeField] bool isAnActionButton=false;
 
     #region Animation
     Animation[] papoter;
@@ -49,10 +62,19 @@ public class T_TempsMort : MonoBehaviour
 
     private void Start()
     {
-        foreach(GameObject button in actions)
+        initiateStats();
+        #region affichage ou non des boutons d'ui
+        foreach (GameObject button in actions)
         {
+            if(button!=null)
             button.SetActive(false);
         }
+        foreach (GameObject fiche in charactersCompleteResume)
+        {
+            if (fiche != null)
+                fiche.SetActive(false);
+        }
+        ChallengeButton.SetActive(false);
         
         Es = FindObjectOfType<EventSystem>();
         GameManager.instance.ChangeActionMap("TempsMort");
@@ -62,14 +84,107 @@ public class T_TempsMort : MonoBehaviour
 
         //Lance le dialogue.
         //StartIntroduction();
+        #endregion
     }
     #region input
-   
+    public void AffichageMiniCartePerso()
+    {
+        for(int i=0;i<charactersLittleResume.Length;i++)
+        {
+            if (currentButton == charactersButton[i])
+            {
+                charactersLittleResume[i].SetActive(true);
+            }
+            else
+                charactersLittleResume[i].SetActive(false);
+        }
+    }
+    public void AffichageCarteCompletePerso()
+    {
+        for (int i = 0; i < charactersCompleteResume.Length; i++)
+        {
+            if (actorActif == characters[i]&&isAnActionButton==true)
+            {
+                charactersCompleteResume[i].SetActive(true);
+            }
+            else
+                charactersCompleteResume[i].SetActive(false);          
+        }
+    }
     public void updateButton()
     {
         currentButton = Es.currentSelectedGameObject;
+        AffichageMiniCartePerso();
+        AffichageCarteCompletePerso();     
     }
-    public void updateSmallCharacterCard()
+    public void SpawnActions()
+    {
+        isAnActionButton = true;
+        for(int i=0;i<charactersButton.Length;i++)
+        {
+            if(charactersButton[i]==currentButton)
+            {
+                characterHasPlayed[i] = true;
+                actorActif = characters[i];
+            }
+        }
+        foreach (GameObject button in actions)
+        {
+            if (button != null)
+                button.SetActive(true);
+        }
+        foreach(GameObject characters in charactersButton)
+        {
+            if (characters != null)
+            {
+                Debug.Log(characters);
+                characters.SetActive(false);
+            }
+                
+        }
+        Es.SetSelectedGameObject(actions[0]);
+        updateButton();
+        Debug.Log(currentButton);
+    }
+    public void SpawnCharactersButtons()
+    {
+        isAnActionButton = false;
+        actorActif = null;
+        foreach (GameObject button in actions)
+        {
+            if (button != null)
+                button.SetActive(false);
+        }
+        /*foreach (GameObject characters in charactersButton)
+        {
+            if (characters != null)
+                characters.SetActive(true);
+        }*/
+        for (int i= 0;i< charactersButton.Length;i++)
+        {
+            if(characterHasPlayed[i]!=true)
+            {
+                charactersButton[i].SetActive(true);
+                Es.SetSelectedGameObject(charactersButton[i]);
+            }
+        }
+        if(characterHasPlayed[0]==true&& characterHasPlayed[1] == true && characterHasPlayed[2] == true)
+        {
+            SpawnChallengeButton();   
+        }
+
+        //Es.SetSelectedGameObject(charactersButton[0]);
+       
+        updateButton();
+        Debug.Log(currentButton);
+    }
+    public void SpawnChallengeButton()
+    {
+        ChallengeButton.SetActive(true);
+        Es.SetSelectedGameObject(ChallengeButton);
+        updateButton();
+    }
+    public void GoToChallenge()
     {
 
     }
@@ -91,15 +206,7 @@ public class T_TempsMort : MonoBehaviour
 
         }
     }
-    public void Validate(InputAction.CallbackContext context)
-    {
-        if (!context.performed) { return; }
 
-        if (context.performed)
-        {
-
-        }
-    }
 
 
     #endregion
@@ -169,6 +276,13 @@ public class T_TempsMort : MonoBehaviour
      for(int i=0;i<characters.Length;i++)
         {
             Instantiate(characters[i], listCharactersPositions[i].InitialPosition.gameObject.transform);
+        }
+    }
+    void initiateStats()
+    {
+      for(int i=0;i<characters.Length;i++)
+        {
+            littleCharactersSpecs[i].text = "PV " + characters[i].GetComponent<C_Actor>().dataActor.stressMax + "\nEnergy "+ characters[i].GetComponent<C_Actor>().dataActor.energyMax;
         }
     }
     //retour au choix de personnage et disparition des boutons de choix
