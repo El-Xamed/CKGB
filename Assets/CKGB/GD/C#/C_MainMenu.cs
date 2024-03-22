ec38e69a62f92f4f93a8cc4d4e85d4c8a85f4944
Paul Richier <paul98cam82@gmail.com>
2024-03-22 13:52:23 +0100

    Fix menu Pi (manque anim "Pressed" + le dev pour toutes les interactions avec les boutons)

---
+++ Assets/CKGB/GD/C#/C_MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_MainMenu : MonoBehaviour
+{
+    [SerializeField] SO_Challenge firthChallenge;
+
+    public void NewParty()
+    {
+
+    }
+
+    public void OpenSave()
+    {
+
+    }
+
+    public void OpenCredits()
+    {
+
+    }
+
+    public void LeaveGame()
+    {
+
+    }
+
+    //Focntion à dev plus tard. Sert pour tous les menu.
+    public void Back()
+    {
+        
+    }
+}
