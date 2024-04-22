2edbfcedf79691931ec625539cc2a02e1f771a72
El-Xamed <maximetessier47@gmail.com>
2024-04-22 15:08:32 +0200

    script tree character to replace useless data

---
+++ Assets/CKGB/GD/C#/TempsMort/C_Tree.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Tree : MonoBehaviour
+{
+    public GameObject c_button;
+    public GameObject c_cursor;
+
+    public GameObject p_button;
+    public GameObject p_cursor;
+
+}
