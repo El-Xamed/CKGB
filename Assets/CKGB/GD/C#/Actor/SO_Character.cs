6168bb342eba87cd1be671f2c08516ee3c4f37d7
El-Xamed <maximetessier47@gmail.com>
2024-04-23 09:07:02 +0200

    add team and smoller

--- Assets/CKGB/GD/C#/Actor/SO_Character.cs
+++ Assets/CKGB/GD/C#/Actor/SO_Character.cs
@@ -37,6 +37,7 @@ public class SO_Character : ScriptableObject
 
     [Header("Tempsmort (Sprite)")]
     public Sprite ProfilPhoto;
+    public Sprite smaller;
     public Sprite MapTmSprite;
     public Sprite NormalBulle;
     public Sprite ThinkBulle;
