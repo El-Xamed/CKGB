1f39c99e6106b5232d8ca46ef21ad8c2dc6d1554
Paul Richier <paul98cam82@gmail.com>
2024-06-10 11:55:32 +0200

    add mon script dans le tuto

--- Assets/CKGB/GD/C#/Challenge/C_Tuto.cs
+++ Assets/CKGB/GD/C#/Challenge/C_Tuto.cs
@@ -14,39 +14,33 @@ public class C_Tuto : MonoBehaviour
         GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface(C_Interface.Interface.Neutre);
     }
 
-     #J'aimerais que cette fonction fasse l'animation du menu de trait qui s'ouvre.
+     //J'aimerais que cette fonction fasse l'animation du menu de trait qui s'ouvre.
     public void OpenTraits()
     {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
+        GetComponentInParent<C_Challenge>().GetInterface().GoTraits();
     }
 
-    #J'aimerais que cette fonction fasse l'animation du menu d'action qui s'ouvre.
+    //J'aimerais que cette fonction fasse l'animation du menu d'action qui s'ouvre.
     public void OpenActions()
     {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
+        GetComponentInParent<C_Challenge>().GetInterface().GoAction();
     }
 
-    #J'aimerais que cette fonction fasse l'animation du menu de trait qui se ferme.
-    public void CloseTraits()
+    //J'aimerais que cette fonction fasse l'animation du menu de trait qui se ferme.
+    public void CloseInterface()
     {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
+        GetComponentInParent<C_Challenge>().GetInterface().GoBack();
     }
 
-    #J'aimerais que cette fonction fasse l'animation du menu d'action qui se ferme.
-    public void CloseActions()
-    {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
-    }
-
-    #J'aimerais que cette fonction fasse l'animation de la jauge de calme qui descend jusqu'au rouge.
+    //J'aimerais que cette fonction fasse l'animation de la jauge de calme qui descend jusqu'au rouge.
      public void CalmDown()
     {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
+        //GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
     }
 
-    #J'aimerais que cette fonction fasse l'animation de la jauge de calme qui remonte full.
+    //J'aimerais que cette fonction fasse l'animation de la jauge de calme qui remonte full.
      public void CalmUp()
     {
-        GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
+        //GetComponentInParent<C_Challenge>().GetInterface().SetCurrentInterface();
     }
 }
